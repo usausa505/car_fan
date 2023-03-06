967d6a6a7e0a6328e440c5c2aa3138b78dd04ce7
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-06 14:09:11 +0000

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'posts/edit'
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :admin do
-    get 'users/edit'
-    get 'users/index'
-    get 'users/show'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
+  # ユーザー用
+  # URL /users/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
   namespace :public do
     get 'homes/top'
     get 'homes/about'
@@ -27,12 +21,6 @@ Rails.application.routes.draw do
     get 'users/index'
     get 'users/show'
   end
-  # ユーザー用
-  # URL /users/sign_in ...
-  devise_for :users,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
   
   
   
@@ -44,6 +32,13 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    root to: 'homes#top'
+    resources :posts, only: [:show, :index, :edit, :destroy]
+    resources :users, only: [:show, :index, :edit, :update]
+    
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
