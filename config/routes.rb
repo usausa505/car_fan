ebbd51366d71c0a815b5faa285cf79b0d8c38b3e
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-07 07:57:42 +0000

    Edit route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,33 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'posts/edit'
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :public do
-    get 'users/edit'
-    get 'users/index'
-    get 'users/show'
+ scope module: :public do
+  root to: 'homes#top'
+  get "/about"=>"homes#about"
+  get 'relationships/followings'
+  get 'relationships/followers'
+  get "search" => "searches#search"
+  
+  resources :posts, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   
+  resources :users, only: [:index,:show,:edit,:update] do
+    # 以下フォロー機能の実装
+    resource :relationships, only: [:create, :destroy] 
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
+  resource :users,only: [] do
+    get "my_page"=>"users#show"
+    get "information/edit"=>"users#edit"
+    patch "information"=>"users#update"
+    get "confirm_withdraw"=>"users#confirm_withdraw"
+    patch "withdraw"=>"users#withdraw"
+  end
+end
   
   
   
@@ -35,9 +47,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'homes#top'
-    resources :posts, only: [:show, :index, :edit, :destroy]
-    resources :users, only: [:show, :index, :edit, :update]
-    
+    resources :posts, only: [:show, :index, :edit, :destroy] # destroyは不適切な投稿を削除するためのモノ
+    resources :users, only: [:show, :index, :edit, :update]　# updateはユーザーの利用状況を更新するためのモノ
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
