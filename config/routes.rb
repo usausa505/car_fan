e626a3f37a8b1aaa659becac30570784ebcafd77
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-07 08:44:52 +0000

    Edit route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,34 +7,29 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
- scope module: :public do
-  root to: 'homes#top'
-  get "/about"=>"homes#about"
-  get 'relationships/followings'
-  get 'relationships/followers'
-  get "search" => "searches#search"
+  scope module: :public do
+    root to: 'homes#top'
+    get "/about"=>"homes#about"
+    get "search" => "searches#search"
   
-  resources :posts, only: [:index, :show, :edit, :create, :destroy, :update] do
-    resources :post_comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
+    resources :posts, only: [:index, :show, :edit, :create, :destroy, :update] do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
+  
+    resources :users, only: [:index,:edit,:update] do
+      get "my_page"=>"users#show"
+      # 以下フォロー機能の実装
+      resource :relationships, only: [:create, :destroy] 
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+  
+    resource :users, only: [] do
+      get "confirm_withdraw"=>"users#confirm_withdraw"
+      patch "withdraw"=>"users#withdraw"
+    end
   end
-  
-  resources :users, only: [:index,:show,:edit,:update] do
-    # 以下フォロー機能の実装
-    resource :relationships, only: [:create, :destroy] 
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
-  end
-  
-  resource :users,only: [] do
-    get "my_page"=>"users#show"
-    get "information/edit"=>"users#edit"
-    patch "information"=>"users#update"
-    get "confirm_withdraw"=>"users#confirm_withdraw"
-    patch "withdraw"=>"users#withdraw"
-  end
-end
-  
   
   
   
@@ -48,7 +43,7 @@ end
   namespace :admin do
     root to: 'homes#top'
     resources :posts, only: [:show, :index, :edit, :destroy] # destroyは不適切な投稿を削除するためのモノ
-    resources :users, only: [:show, :index, :edit, :update]　# updateはユーザーの利用状況を更新するためのモノ
+    resources :users, only: [:index,:show,:edit,:update] # updateはユーザーステータスを変更するためのモノ
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
