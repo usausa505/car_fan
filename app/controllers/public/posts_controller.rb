32956da9cc08cd8f46c08a3f5a751a87ac699b1f
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-08 03:02:13 +0000

    Edit user,post model

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,62 @@
 class Public::PostsController < ApplicationController
-  def edit
-  end
+  
+  before_action :is_matching_login_user_on_post, only: [:edit, :update, :destroy]
 
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post.id), notice: "投稿に成功しました！"
+    else
+      @user = current_user
+      @posts = Post.all
+      render "index"
+    end
+  end
+  
   def index
+    @post = post.new
+    @user = current_user
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @new_post = Post.new
+    @post_comment = PostComment.new
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "編集に成功しました！"
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "投稿の削除に成功しました！"
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+  
+  def is_matching_login_user_on_post
+    @post = Post.find(params[:id])
+    @user = @post.user
+    unless @user == current_user
+      redirect_to posts_path
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+  
+  belongs_to :user
+  validates :title, presence:true, length: {maximum:50}
+  validates :body, presence:true, length: {maximum:200}
+  
+  has_many_attached :image
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+      image
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  
 end
