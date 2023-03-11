61d0e78948ee622cf869949315ddbd37c0339504
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-11 10:33:01 +0000

    コメント機能実装、非同期通信化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,2 +1,21 @@
 class Public::PostCommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    @comment.save
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    PostComment.find(params[:id]).destroy
+  end
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+
 end
