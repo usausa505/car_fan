4019970ab5fbbf7e0ba9ee5a07a69511bd2cd562
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-11 11:39:44 +0000

    レイアウト調整

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,20 @@
 class Post < ApplicationRecord
-  
+
   belongs_to :user
   validates :title, presence:true, length: {maximum:50}
   validates :body, presence:true, length: {maximum:200}
-  
-  FILE_NUMBER_LIMIT = 4
 
+  FILE_NUMBER_LIMIT = 4
   validate :validate_number_of_files
-  
-  
+
+
   has_many_attached :images
   has_many :post_comments, dependent: :destroy
+
+
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
-  
+
   def get_image
     unless images.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -21,16 +22,16 @@ class Post < ApplicationRecord
     end
       images
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   private
-  
+
   def validate_number_of_files
     return if images.length <= FILE_NUMBER_LIMIT
     errors.add(:images, "に添付できる画像は#{FILE_NUMBER_LIMIT}件までです。")
   end
-  
+
 end
