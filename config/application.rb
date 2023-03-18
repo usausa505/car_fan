aec2b294228d2a490ebf8f06ef2628be4b1880a7
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-18 13:27:06 +0000

    バリデーション実装中

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module CarFan
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
+    #日本語化させるための記述
+
   end
 end
