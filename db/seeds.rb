f6cd27c32543083fe14072873c74155e4f355b5d
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-22 13:59:45 +0000

    テストデータ作成ほぼ完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,6 +36,75 @@ post_data.update(images: [image1, image2, image3, image4])
 
 post_data = Post.create!(
   user_id: user.id,
-  title: 'サンプル投稿',
-  body: 'サンプルだよ'
+  title: 'GT-Rについて',
+  body: 'R36が出るとしたら、EV化されるんだろうか…'
+)
+
+
+post_data = Post.create!(
+  user_id: user.id,
+  title: 'GT-R nismoについて',
+  body: '値段にさえ目をつむれば、R35の集大成と言えるレベルだよな'
+)
+
+user = User.find(2)
+
+post_data = Post.create!(
+  user_id: user.id,
+  title: 'NSX最高！',
+  body: '日本のスーパーカーというなら、ホンダはNSXでしょ'
+)
+
+image1 = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/post_nsx1.jpg"), filename: "post_nsx1.jpg")
+image2 = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/post_nsx2.jpg"), filename: "post_nsx2.jpg")
+post_data.update(images: [image1, image2])
+
+post_data = Post.create!(
+  user_id: user.id,
+  title: 'NSXの最後',
+  body: '2代目NSXも生産終了しちゃったなぁ'
+)
+
+user = User.find(3)
+
+post_data = Post.create!(
+  user_id: user.id,
+  title: '新型Zの雄姿',
+  body: 'イカズチイエローのネーミング、良きかな'
+)
+
+image1 = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/post_z1.jpg"), filename: "post_z1.jpg")
+image2 = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/post_z2.jpg"), filename: "post_z2.jpg")
+image3 = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/post_z3.jpg"), filename: "post_z3.jpg")
+image4 = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/post_z4.jpg"), filename: "post_z4.jpg")
+post_data.update(images: [image1, image2, image3, image4])
+
+post_data = Post.create!(
+  user_id: user.id,
+  title: 'Zが欲しい…',
+  body: 'バージョンSTいいよなぁ…高いけど'
+)
+
+user = User.find(4)
+
+post_data = Post.create!(
+  user_id: user.id,
+  title: 'GRGarage行ってきました',
+  body: 'スープラの他にもヤリスとか86もあったわ'
+)
+
+image1 = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/post_grsupra1.jpg"), filename: "post_grsupra1.jpg")
+image2 = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/post_grsupra2.jpg"), filename: "post_grsupra2.jpg")
+post_data.update(images: [image1, image2])
+
+post_data = Post.create!(
+  user_id: user.id,
+  title: 'MTの良いところ',
+  body: 'MTの良いところは…かっこいいから！w'
+)
+
+post_data = Post.create!(
+  user_id: user.id,
+  title: 'Zも良いよね',
+  body: '同じ価格帯だと、Zも普通にカッコイイんだよなぁ'
 )
