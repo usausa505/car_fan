02fb6707517c0d09f4477b0755bed06742374a86
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-03-26 09:26:13 +0000

    ログイン制限機能調整

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -29,9 +29,13 @@ class Public::UsersController < ApplicationController
   end
 
   def favorites
-    favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)
-    @favorite_posts = Post.find(favorites)
-    @favorite_posts = Kaminari.paginate_array(@favorite_posts).page(params[:page]).per(10)
+    if @user == current_user
+      favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)
+      @favorite_posts = Post.find(favorites)
+      @favorite_posts = Kaminari.paginate_array(@favorite_posts).page(params[:page]).per(10)
+    else
+      redirect_to user_my_page_path(current_user)
+    end
   end
 
   def confirm_withdraw
